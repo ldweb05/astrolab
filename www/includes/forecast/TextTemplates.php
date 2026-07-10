c0f06652eb15169060a530edaf9649c37327765b
Lorenzo Diana <ldweb05@gmail.com>
2026-07-10 11:23:24 +0200

    feat: aggiunti template narrativi

---
+++ www/includes/forecast/TextTemplates.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+final class TextTemplates
+{
+    public const INTRO = [
+
+        'carriera'      => 'L\'anno pone una forte enfasi sulla crescita professionale e sul raggiungimento di obiettivi concreti.',
+        'lavoro'        => 'Il lavoro richiederà attenzione costante e offrirà occasioni per consolidare la propria posizione.',
+        'amore'         => 'La sfera affettiva sarà uno dei temi dominanti dell\'anno.',
+        'relazioni'     => 'I rapporti con gli altri avranno un ruolo determinante nelle scelte future.',
+        'salute'        => 'La salute merita attenzione e una gestione equilibrata delle energie.',
+        'denaro'        => 'L\'aspetto economico accompagnerà molte decisioni importanti.',
+        'casa'          => 'Casa e famiglia saranno al centro di numerosi cambiamenti.',
+        'famiglia'      => 'L\'ambiente familiare richiederà presenza e partecipazione.',
+        'viaggi'        => 'Viaggi e spostamenti potranno aprire nuove prospettive.',
+        'studio'        => 'Lo studio e l\'aggiornamento personale saranno favoriti.',
+        'amicizie'      => 'Nuove amicizie e collaborazioni potranno rivelarsi preziose.',
+        'progetti'      => 'Molti progetti potranno finalmente prendere forma.',
+        'trasformazione'=> 'L\'anno rappresenta un periodo di profonda trasformazione personale.',
+        'prove'         => 'Alcune prove richiederanno pazienza e capacità di adattamento.',
+
+    ];
+}
