b7d8b8cae306705778232a78cb040f94f2d7dfb4
Lorenzo Diana <ldweb05@gmail.com>
2026-07-12 06:56:36 +0200

    feat: aggiunge regola Marte in sesta

--- www/includes/forecast/RuleRegistry.php
+++ www/includes/forecast/RuleRegistry.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 require_once __DIR__.'/rules/Rule0001_Jupiter10.php';
 require_once __DIR__.'/rules/Rule0002_Saturn12.php';
+require_once __DIR__.'/rules/Rule0003_Mars6.php';
 
 /**
  * Registro centrale delle regole astrologiche.
@@ -17,6 +18,7 @@ final class RuleRegistry
         return [
             new Rule0001_Jupiter10(),
             new Rule0002_Saturn12(),
+            new Rule0003_Mars6(),
         ];
     }
 }
