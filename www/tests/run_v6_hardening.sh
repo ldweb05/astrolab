6ea40380e42aad8722a4404a2ded1313935deefa
Lorenzo Diana <ldweb05@gmail.com>
2026-07-14 08:35:33 +0200

    test(v6): validate PostgreSQL runtime connectivity

--- www/tests/run_v6_hardening.sh
+++ www/tests/run_v6_hardening.sh
@@ -61,6 +61,7 @@ assert_git_clean() {
 
 run_test "$TESTS/test_astronomical_backend_architecture.php"
 run_test "$TESTS/test_runtime_environment.php"
+run_test "$TESTS/test_database_environment.php"
 run_test "$TESTS/test_annual_summary_builder.php"
 run_test "$TESTS/test_executive_summary_narrative.php"
 run_test "$TESTS/test_theme_summary_narrative.php"
