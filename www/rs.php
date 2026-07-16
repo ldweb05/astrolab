21f4d32efe68b8da6ad2830afcd65f7f65b2ebdf
Lorenzo Diana <ldweb05@gmail.com>
2026-07-16 06:26:42 +0200

    refactor(ui): centralize shared CSS and enable mobile viewport

--- www/rs.php
+++ www/rs.php
@@ -85,6 +85,7 @@ if ($soggetto) {
 <html lang="it">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rivoluzione Solare</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/print.css">
